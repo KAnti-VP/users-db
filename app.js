706f5d962192430918019ed3df43b63c7d9edf6b
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2025-04-01 14:13:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,71 @@
-import express from 'express'
-import { initializeDatabase } from './util/database.js'
+import express from "express";
+import { initializeDatabase, dbAll, dbGet, dbRun } from "./util/database.js";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-// 
-// 
+app.get("/users", async (req, res) => {
+  const users = await dbAll("SELECT * FROM users");
+  res.status(200).json(users);
+});
 
+app.get("/users/:id", async (req, res) => {
+  const user = await dbGet("SELECT * FROM users WHERE id = ?;", [
+    req.params.id,
+  ]);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  res.status(200).json(user);
+});
 
-app.use((req, res, next, err) => {
-    if (err) {
-        res.status(500).json({message: `Error: ${err.message}`})
-    }
+app.post('/users', async (req, res) => {
+  const {name, age} = req.body
+  if (!name || !age) {
+    return res.status(404).json({message: 'Invalid data'})
+  }
+  const result = await dbRun(`INSERT INTO users 
+    (name, age) VALUES (?, ?);`, [name, age])
+  res.status(201).json({id: result.lastID, name, age })
 })
 
+app.put('/users/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await dbGet("SELECT * FROM users WHERE id = ?;", [id]);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  const {name, age} = req.body
+  if (!name || !age) {
+    return res.status(404).json({message: 'Missing data'})
+  }
+  dbRun("UPDATE users SET name = ?, age = ? WHERE id = ?;",
+    [name, age, id]
+  )
+  res.status(200).json({id, name, age});
+})
+
+app.delete('/users/:id', async (req, res) => {
+  const id = req.params.id
+  const user = await dbGet("SELECT * FROM users WHERE id = ?;", [id]);
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  dbRun("DELETE FROM users WHERE id = ?;", [id])
+  res.status(200).json({message: 'Delete successful'})
+})
+
+app.use((req, res, next, err) => {
+  if (err) {
+    res.status(500).json({ message: `Error: ${err.message}` });
+  }
+});
+
 async function startServer() {
-    await initializeDatabase();
-    app.listen(3000, () => {
-        console.log('Server runs on port 3000')
-    })
+  await initializeDatabase();
+  app.listen(3000, () => {
+    console.log("Server runs on port 3000");
+  });
 }
 
 startServer();
